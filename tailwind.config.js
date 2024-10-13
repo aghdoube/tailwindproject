f8a0cabcea3fd99acc9ea3141e9dab0f8cc72443
Hayat <aghdoubehayat@gmail.com>
2024-10-13 22:14:18 +0200

    Initial commit with HTML files and assets

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Adjust this to include your HTML and JS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
